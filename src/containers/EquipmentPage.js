3904723f232b4ad98473d15006f65f7f5538bfb1
Pete Hanner <petehanner@gmail.com>
2019-10-17 15:55:24 -0400

    refactor eqt container

--- src/containers/EquipmentPage.js
+++ src/containers/EquipmentPage.js
@@ -3,26 +3,38 @@ import { Grid, Segment } from 'semantic-ui-react'
 import IconContainer from './IconContainer'
 import DetailContainer from './DetailContainer'
 
-const EquipmentPage = (props) => {
-  const eqtType = props.match.params.type
-  const eqtArray = props[`${eqtType}`]
-  console.log(eqtArray);
+class EquipmentPage extends React.Component {
 
-  return(
-    <Segment placeholder>
-      <p>{`This is the equipment page for ${eqtType}`}</p>
-      <Segment>
-        <Grid columns={2} divided>
-          <Grid.Column width={10}>
-            <IconContainer />
-          </Grid.Column>
-          <Grid.Column width={6} align='center'>
-            <DetailContainer />
-          </Grid.Column>
-        </Grid>
+  constructor() {
+    super()
+    this.state = {
+      equipment: []
+    }
+  }
+
+  componentDidMount() {
+    const eqtType = this.props.match.params.type
+    const eqtArray = this.props[`${eqtType}`]
+    this.setState({equipment: eqtArray})
+  }
+
+  render() {
+    return(
+      <Segment placeholder>
+        <p>{`This is the equipment page for`}</p>
+        <Segment>
+          <Grid columns={2} divided>
+            <Grid.Column width={10}>
+              <IconContainer />
+            </Grid.Column>
+            <Grid.Column width={6} align='center'>
+              <DetailContainer />
+            </Grid.Column>
+          </Grid>
+        </Segment>
       </Segment>
-    </Segment>
-  )
+    )
+  }
 }
 
 export default EquipmentPage
