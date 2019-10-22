dc7a02d6c3eaac929a23ecf7b8451716f9c57934
Pete Hanner <petehanner@gmail.com>
2019-10-22 16:29:23 -0400

    More styling

--- src/containers/BattleInterface.js
+++ src/containers/BattleInterface.js
@@ -12,13 +12,13 @@ class BattleInterface extends React.Component {
       :
       <Fragment>
       <Grid.Column textAlign='center'>
-        <h4 className='midevil-font'>Equipment</h4>
+        <h4 className='midevil-font header'>Equipment</h4>
         <p className='midevil-font'>Weapon: {this.props.userWeapon.name}</p>
         <p className='midevil-font'>Armor: {this.props.userArmor.name}</p>
         <p className='midevil-font'>Horse: {this.props.userHorse.name}</p>
       </Grid.Column>
         <Grid.Column textAlign='center'>
-          <h3 className='midevil-font' >PLAYER</h3>
+          <h3 className='midevil-font header' >PLAYER</h3>
           <Progress progress='value' value={this.props.userHp.toFixed(2)} total={100}  indicating>
           HP
           </Progress>
@@ -28,13 +28,13 @@ class BattleInterface extends React.Component {
         </Grid.Column>
         <Grid.Column textAlign='center' width={3.75}>
           <div className='data-div' align="center" >
-            <p className='midevil-font'>{this.props.userAtkMsg}</p>
-            <p className='midevil-font'>{this.props.oppAtkMsg}</p>
+            <p className='midevil-font log'>{this.props.userAtkMsg}</p>
+            <p className='midevil-font log'>{this.props.oppAtkMsg}</p>
           </div>
           <Image id='joust-logo' alt='JOUST!' src='https://i.imgur.com/gYyasxF.png' onClick={this.props.charging ? null : this.props.joust} />
         </Grid.Column>
         <Grid.Column textAlign='center'>
-          <h3 className='midevil-font'>OPPONENT</h3>
+          <h3 className='midevil-font header'>OPPONENT</h3>
           <Progress progress='value' value={this.props.opponentHp.toFixed(2)} total={100}  indicating>
           HP
           </Progress>
@@ -43,7 +43,7 @@ class BattleInterface extends React.Component {
           <p className='midevil-font'>Speed Modifier: {this.props.opponentSpd.toFixed(2)}</p>
         </Grid.Column>
         <Grid.Column textAlign='center'>
-          <h4 className='midevil-font'>Equipment</h4>
+          <h4 className='midevil-font header'>Equipment</h4>
           <p className='midevil-font'>Weapon: {this.props.opponentWeapon.name}</p>
           <p className='midevil-font'>Armor: {this.props.opponentArmor.name}</p>
           <p className='midevil-font'>Horse: {this.props.opponentHorse.name}</p>
