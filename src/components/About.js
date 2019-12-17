ddf03caef0f4c5ff1e0d11bff3bf41c14488e1e2
Pete Hanner <petehanner@gmail.com>
2019-12-17 15:02:36 -0500

    secure link target

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import {Button} from 'semantic-ui-react'
-import {NavLink} from 'react-router-dom'
+import { Button } from 'semantic-ui-react'
+import { NavLink } from 'react-router-dom'
 
 const About = () => {
-  return(
+  return (
     <React.Fragment>
       <h1 class='welcome'>Welcome to Joust!</h1>
       <h3 class='how'>How to Play</h3>
@@ -14,10 +14,10 @@ const About = () => {
       <p class='about-info'>- Attack Modifier: The chance this weapon will hit, as a % out of 100</p>
       <p class='about-info'>- Defense Modifier: How much this weapon helps or hurts your defense, as a % out of 100</p>
       <p class='about-info'>- Speed Modifier: How much this weapon affects your speed. The faster you are, the more damage you deal.</p>
-      <p class='about-info'><img alt='shield icon'src={require('../assets/images/icons/shield.png')} className='about-page-icon'/> <strong>This button will lead you to a selection of ARMOR to choose from for your battle</strong></p>
+      <p class='about-info'><img alt='shield icon' src={require('../assets/images/icons/shield.png')} className='about-page-icon' /> <strong>This button will lead you to a selection of ARMOR to choose from for your battle</strong></p>
       <p class='about-info'>- Base Defense: The likelihood this armor will protext you from an attack, as a % out of 100</p>
       <p class='about-info'>- Speed Modifier: How much this armor affects your speed. The faster you are, the more damage you deal.</p>
-      <p class='about-info'><img alt='horse icon'src={require('../assets/images/icons/horse.png')} className='about-page-icon'/><strong>This button will lead you to a selection of HORSES to choose from for your battle</strong></p>
+      <p class='about-info'><img alt='horse icon' src={require('../assets/images/icons/horse.png')} className='about-page-icon' /><strong>This button will lead you to a selection of HORSES to choose from for your battle</strong></p>
       <p class='about-info'>- Attack Modifier: The amount this horse boosts or hinders your % chance of attack success</p>
       <p class='about-info'>- Defense Modifier: The amount this horse boosts or hinders your % chance of defense success</p>
       <p class='about-info'>- Speed Modifier: How fast this horse can move compared to average. The faster you are, the more damage you deal.</p>
@@ -37,17 +37,17 @@ const About = () => {
       <p class='about-info'>- If the User attack and Opponent defense both fail:</p>
       <p class='about-info'>+ No one takes any damage and you both swallow your pride before trying to do better next round</p>
       <br />
-        <p class='about-info'>- The same rules apply in reverse for your Opponent</p>
+      <p class='about-info'>- The same rules apply in reverse for your Opponent</p>
 
       <br></br>
       <Button color='yellow'>
-        <NavLink to='/' style={{color: 'white', textDecoration: 'none'}} >
+        <NavLink to='/' style={{ color: 'white', textDecoration: 'none' }} >
           Back to the Game
         </NavLink>
       </Button>
       <br />
       <div id='attribution'>
-        <strong><p>Created by <a href='https://github.com/ggarciag24' target="_blank" >German Garcia Gonzalez</a> and <a href='https://github.com/PeteHanner' target="_blank" >Pete Hanner</a></p></strong>
+        <strong><p>Created by <a href='https://github.com/ggarciag24' target="_blank" rel="noopener noreferrer">German Garcia Gonzalez</a> and <a href='https://github.com/PeteHanner' target="_blank" rel="noopener noreferrer">Pete Hanner</a></p></strong>
         <br></br>
         <p> Created my free logo at <a href='https://my.logomakr.com'>LogoMakr.com</a></p>
         <p>Jousting Knight icon by Estherhase</p>
